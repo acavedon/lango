eda57f90ba81fe12b0994f086d8dd4989ca4a30a
Alexandre Cavedon <alexandre.cavedon@gmail.com>
2011-06-09 11:51:49 -0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc3'
+gem 'sqlite3'
+gem 'devise', '1.1.4'
+gem 'mail'
+gem 'haml-rails'
+
+# Asset template engines
+gem 'sass-rails', "~> 3.1.0.rc"
+gem 'coffee-script'
+gem 'uglifier'
+gem 'jquery-rails'
+
+if RUBY_PLATFORM =~ /linux/i
+  gem 'rb-inotify'
+  gem 'libnotify'
+  gem 'therubyracer'
+end
+
+group :development do
+  gem 'ruby-debug19', :require => 'ruby-debug'
+end
+
+group :test do
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lango::Application.config.secret_token = '994f8a7e6399a7dfeddfb496df10b85cf8bb9a45bc7faddd7842cd5c78af83e2476c1903ea70e57872ab3ecdd5e0b68baae731c51e0e4be62c0b533736bb53ad'
