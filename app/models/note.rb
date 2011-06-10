2c918067dcecd47fc4ec579b08a07da1ec80c883
Alexandre Cavedon <alexandre.cavedon@gmail.com>
2011-06-10 16:53:00 -0300

    Fixed Admin Style

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,4 @@
 class Note < ActiveRecord::Base
   validates :title, :length => 3..255, :presence => true
+  validates :body, :presence => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110610122657) do
+ActiveRecord::Schema.define(:version => 20110610132629) do
 
   create_table "notes", :force => true do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "tests", :force => true do |t|
-    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
